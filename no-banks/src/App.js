77106d684bbfd4e5ba01b74d00ada547a195306d
Bruno Oliveira <brunooliveira.unb@gmail.com>
2023-10-27 00:47:31 -0300

    inicializando projeto

---
+++ no-banks/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{fontSize: "90px"}}>
+          No Banks.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
