c27e52dabdf6565b69796ec67c1e5bc920105a54
Bruno Oliveira <brunooliveira.unb@gmail.com>
2023-10-28 13:03:57 -0300

    withdrawal frontend

--- no-banks/src/pages/Withdrawal/Withdrawal.js
+++ no-banks/src/pages/Withdrawal/Withdrawal.js
@@ -1,19 +1,32 @@
-import { Button } from "reactstrap";
+import { Button, Input, InputGroup, InputGroupText, Label } from "reactstrap";
 import "./Withdrawal.css"
+
+import { data } from "../../utils/data";
 
 const Withdrawal = () => {
   return (
 
     <div className="withdrawal">
-      <div>
-        <p>
-          <input
+     
+      <div className="withdrawal-input">
+        <Label>DIGITE O VALOR QUE DESEJA SACAR</Label>
+        <InputGroup >
+          <InputGroupText>$</InputGroupText>
+          <Input
             placeholder="Enter Deposit"
             min="3"
             max="100"
           />
           <Button className="deposit-button">Sacar</Button>
-        </p>
+        </InputGroup>
+      </div>
+      <div>
+        <p style={{ textAlign: "center", marginTop: "2em", color: "red" }}>NOTAS DISPONÍVEIS:
+        <li>R$1,00&nbsp;&nbsp;&nbsp;&nbsp;R$2,00</li> 
+        <li>R$5,00&nbsp;&nbsp;&nbsp;&nbsp; R$10,00</li>
+        <li>R$20,00&nbsp;&nbsp;&nbsp;&nbsp;R$50,00</li>
+        <li>R$100,00&nbsp;&nbsp;&nbsp;&nbsp;R$200,00</li>
+        <li></li></p>
       </div>
     </div>
   );
---
+++ no-banks/src/utils/data.js
@@ -0,0 +1,3 @@
+export const data = [ {
+    saldo: '1000'
+}]
